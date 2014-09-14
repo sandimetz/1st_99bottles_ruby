c9f7dd0e0909b6cd6eeb636c4a92e1668159d228
Katrina Owen <katrina.owen@gmail.com>
2014-09-14 13:19:24 -0700

    Shameless Green - 08
    Implement song by calling verses.

--- test/bottles_test.rb
+++ test/bottles_test.rb
@@ -76,7 +76,6 @@ VERSES
   end
 
   def test_the_whole_song
-    skip
     expected = <<-SONG
 99 bottles of beer on the wall, 99 bottles of beer.
 Take one down and pass it around, 98 bottles of beer on the wall.
