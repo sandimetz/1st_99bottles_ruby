fba4a061fbb336cf2a13ffa5af60c34d52c1fcf2
Katrina Owen <katrina.owen@gmail.com>
2014-09-14 13:16:17 -0700

    Shameless Green - 05
    Final special case of verse.

--- test/bottles_test.rb
+++ test/bottles_test.rb
@@ -43,7 +43,6 @@ Take it down and pass it around, no more bottles of beer on the wall.
   end
 
   def test_verse_0
-    skip
     expected = <<-VERSE
 No more bottles of beer on the wall, no more bottles of beer.
 Go to the store and buy some more, 99 bottles of beer on the wall.
