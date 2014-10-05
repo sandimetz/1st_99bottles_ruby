6e93cbf25a81f0acc8e83b36f8243f55c9387046
Katrina Owen <katrina.owen@gmail.com>
2014-10-05 15:48:04 -0700

    DRY: clean up after case 1

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -19,11 +19,6 @@ class Bottles
       "no more bottles of beer.\n" +
       "Go to the store and buy some more, " +
       "99 bottles of beer on the wall.\n"
-    when 1
-      "#{quantity(number)} #{container(number)} of beer on the wall, " +
-      "#{quantity(number)} #{container(number)} of beer.\n" +
-      "Take #{pronoun(number)} down and pass it around, " +
-      "#{quantity(number-1)} #{container(number-1)} of beer on the wall.\n"
     else
       "#{quantity(number)} #{container(number)} of beer on the wall, " +
       "#{quantity(number)} #{container(number)} of beer.\n" +
