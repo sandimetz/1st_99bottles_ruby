20a26c9eb864be4cdc2a6bfd193fe1fce8217cc6
Katrina Owen <katrina.owen@gmail.com>
2014-10-05 16:13:30 -0700

    DRY: call quantity with 99

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -19,7 +19,7 @@ class Bottles
       "of beer on the wall, " +
       "#{quantity(number)} #{container(number)} of beer.\n" +
       "#{action(number)}, " +
-      "99 #{container(number-1)} of beer on the wall.\n"
+      "#{quantity(99)} #{container(number-1)} of beer on the wall.\n"
     else
       "#{quantity(number).capitalize} #{container(number)} " +
       "of beer on the wall, " +
