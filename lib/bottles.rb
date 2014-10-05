296f9d79d6d50cc7fc663dc8beaa7320c5eafcc3
Katrina Owen <katrina.owen@gmail.com>
2014-10-05 16:15:09 -0700

    DRY: use successor

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -19,13 +19,15 @@ class Bottles
       "of beer on the wall, " +
       "#{quantity(number)} #{container(number)} of beer.\n" +
       "#{action(number)}, " +
-      "#{quantity(99)} #{container(number-1)} of beer on the wall.\n"
+      "#{quantity(successor(number))} #{container(successor(number))} " +
+      "of beer on the wall.\n"
     else
       "#{quantity(number).capitalize} #{container(number)} " +
       "of beer on the wall, " +
       "#{quantity(number)} #{container(number)} of beer.\n" +
       "#{action(number)}, " +
-      "#{quantity(number-1)} #{container(number-1)} of beer on the wall.\n"
+      "#{quantity(successor(number))} #{container(successor(number))} " +
+      "of beer on the wall.\n"
     end
   end
 
