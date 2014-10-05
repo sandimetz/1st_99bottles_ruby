abc7b5254d5da13295f83c8d63cb9d539faf429e
Katrina Owen <katrina.owen@gmail.com>
2014-10-05 16:16:57 -0700

    DRY: clean up

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -13,22 +13,6 @@ class Bottles
   end
 
   def verse(number)
-    case number
-    when 0
-      "#{quantity(number).capitalize} #{container(number)} " +
-      "of beer on the wall, " +
-      "#{quantity(number)} #{container(number)} of beer.\n" +
-      "#{action(number)}, " +
-      "#{quantity(successor(number))} #{container(successor(number))} " +
-      "of beer on the wall.\n"
-    else
-      "#{quantity(number).capitalize} #{container(number)} " +
-      "of beer on the wall, " +
-      "#{quantity(number)} #{container(number)} of beer.\n" +
-      "#{action(number)}, " +
-      "#{quantity(successor(number))} #{container(successor(number))} " +
-      "of beer on the wall.\n"
-    end
     "#{quantity(number).capitalize} #{container(number)} " +
     "of beer on the wall, " +
     "#{quantity(number)} #{container(number)} of beer.\n" +
