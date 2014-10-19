61bc3f0c1b3ed245b9f889fda91a5197f83db98d
Katrina Owen <katrina.owen@gmail.com>
2014-10-19 15:24:35 -0700

    Add superfluously general example

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,63 +1,63 @@
-# ...
-  # ...
-    # ...
-      # ...
 class Bottles
+  NoMore = lambda do |verse|
+    "No more bottles of beer on the wall, " +
+    "no more bottles of beer.\n" +
+    "Go to the store and buy some more, " +
+    "99 bottles of beer on the wall.\n"
+  end
+
+  LastOne = lambda do |verse|
+    "1 bottle of beer on the wall, " +
+    "1 bottle of beer.\n" +
+    "Take it down and pass it around, " +
+    "no more bottles of beer on the wall.\n"
+  end
+
+  Penultimate = lambda do |verse|
+    "2 bottles of beer on the wall, " +
+    "2 bottles of beer.\n" +
+    "Take one down and pass it around, " +
+    "1 bottle of beer on the wall.\n"
+  end
+
+  Default = lambda do |verse|
+    "#{verse.number} bottles of beer on the wall, " +
+    "#{verse.number} bottles of beer.\n" +
+    "Take one down and pass it around, " +
+    "#{verse.number - 1} bottles of beer on the wall.\n"
+  end
 
   def song
     verses(99, 0)
   end
 
-  def verses(starting, ending)
-    starting.downto(ending).map {|i| verse(i)}.join("\n")
+  def verses(finish, start)
+    (finish).downto(start).map {|verse_number|
+      verse(verse_number) }.join("\n")
   end
 
   def verse(number)
-    "#{quantity(number).capitalize} #{container(number)} " +
-    "of beer on the wall, " +
-    "#{quantity(number)} #{container(number)} of beer.\n" +
-    "#{action(number)}, " +
-    "#{quantity(successor(number))} #{container(successor(number))} " +
-    "of beer on the wall.\n"
+    verse_for(number).text
   end
 
-  def container(number)
-    if number == 1
-      "bottle"
-    else
-      "bottles"
+  def verse_for(number)
+    case number
+    when 0 then Verse.new(number, &NoMore)
+    when 1 then Verse.new(number, &LastOne)
+    when 2 then Verse.new(number, &Penultimate)
+    else        Verse.new(number, &Default)
     end
   end
+end
 
-  def quantity(number)
-    if number == 0
-      "no more"
-    else
-      number.to_s
-    end
+class Verse
+  attr_reader :number
+  def initialize(number, &lyrics)
+    @number = number
+    @lyrics = lyrics
   end
 
-  def action(number)
-    if number == 0
-      "Go to the store and buy some more"
-    else
-      "Take #{pronoun(number)} down and pass it around"
-    end
-  end
-
-  def pronoun(number)
-    if number == 1
-      "it"
-    else
-      "one"
-    end
-  end
-
-  def successor(number)
-    if number == 0
-      99
-    else
-      number - 1
-    end
+  def text
+    @lyrics.call self
   end
 end
