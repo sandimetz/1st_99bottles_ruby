cd3c601d370b409c9a061013289965cc84184d39
Katrina Owen <katrina.owen@gmail.com>
2014-10-19 15:13:45 -0700

    Incomprehensibly concise example

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,63 +1,19 @@
-# ...
-  # ...
-    # ...
-      # ...
 class Bottles
-
   def song
     verses(99, 0)
   end
 
-  def verses(starting, ending)
-    starting.downto(ending).map {|i| verse(i)}.join("\n")
+  def verses(hi, lo)
+    hi.downto(lo).map {|n| verse(n) }.join("\n")
   end
 
-  def verse(number)
-    "#{quantity(number).capitalize} #{container(number)} " +
-    "of beer on the wall, " +
-    "#{quantity(number)} #{container(number)} of beer.\n" +
-    "#{action(number)}, " +
-    "#{quantity(successor(number))} #{container(successor(number))} " +
-    "of beer on the wall.\n"
-  end
-
-  def container(number)
-    if number == 1
-      "bottle"
-    else
-      "bottles"
-    end
-  end
-
-  def quantity(number)
-    if number == 0
-      "no more"
-    else
-      number.to_s
-    end
-  end
-
-  def action(number)
-    if number == 0
-      "Go to the store and buy some more"
-    else
-      "Take #{pronoun(number)} down and pass it around"
-    end
-  end
-
-  def pronoun(number)
-    if number == 1
-      "it"
-    else
-      "one"
-    end
-  end
-
-  def successor(number)
-    if number == 0
-      99
-    else
-      number - 1
-    end
+  def verse(n)
+    "#{n == 0 ? 'No more' : n} bottle#{'s' if n != 1}" +
+    " of beer on the wall, " +
+    "#{n == 0 ? 'no more' : n} bottle#{'s' if n != 1} of beer.\n" +
+    "#{n > 0  ? "Take #{n > 1 ? 'one' : 'it'} down and pass it around"
+              : "Go to the store and buy some more"}, " +
+    "#{n-1 < 0 ? 99 : n-1 == 0 ? 'no more' : n-1} bottle#{'s' if n-1 != 1}"+
+    " of beer on the wall.\n"
   end
 end
