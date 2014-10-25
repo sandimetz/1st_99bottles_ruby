db25e9d441b4a0d0e9c33aad3cfe3e40a64d15cc
Katrina Owen <katrina.owen@gmail.com>
2014-10-24 22:59:21 -0700

    Create a very concrete example

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -8,56 +8,85 @@ class Bottles
     verses(99, 0)
   end
 
-  def verses(starting, ending)
-    starting.downto(ending).map {|i| verse(i)}.join("\n")
+  def verses(bottles_at_start, bottles_at_end)
+    bottles_at_start.downto(bottles_at_end).map do |bottles|
+      verse(bottles)
+    end.join("\n")
   end
 
-  def verse(number)
-    "#{quantity(number).capitalize} #{container(number)} " +
-    "of beer on the wall, " +
-    "#{quantity(number)} #{container(number)} of beer.\n" +
-    "#{action(number)}, " +
-    "#{quantity(successor(number))} #{container(successor(number))} " +
-    "of beer on the wall.\n"
+  def verse(bottles)
+    Round.new(bottles).to_s
+  end
+end
+
+class Round
+  attr_reader :bottles
+  def initialize(bottles)
+    @bottles = bottles
   end
 
-  def container(number)
-    if number == 1
-      "bottle"
+  def to_s
+    challenge + response
+  end
+
+  def challenge
+    bottles_of_beer.capitalize + " " + on_wall + ", " +
+    bottles_of_beer + ".\n"
+  end
+
+  def response
+    go_to_the_store_or_take_one_down + ", " +
+    bottles_of_beer + " " + on_wall + ".\n"
+  end
+
+  def bottles_of_beer
+    "#{anglicized_bottle_count} #{pluralized_bottle_form} of #{beer}"
+  end
+
+  def beer
+    "beer"
+  end
+
+  def on_wall
+    "on the wall"
+  end
+
+  def pluralized_bottle_form
+    last_beer? ? "bottle" : "bottles"
+  end
+
+  def anglicized_bottle_count
+    all_out? ? "no more" : bottles.to_s
+  end
+
+  def go_to_the_store_or_take_one_down
+    if all_out?
+      @bottles = 99
+      buy_new_beer
     else
-      "bottles"
+      lyrics = drink_beer
+      @bottles -= 1
+      lyrics
     end
   end
 
-  def quantity(number)
-    if number == 0
-      "no more"
-    else
-      number.to_s
-    end
+  def buy_new_beer
+    "Go to the store and buy some more"
   end
 
-  def action(number)
-    if number == 0
-      "Go to the store and buy some more"
-    else
-      "Take #{pronoun(number)} down and pass it around"
-    end
+  def drink_beer
+    "Take #{it_or_one} down and pass it around"
   end
 
-  def pronoun(number)
-    if number == 1
-      "it"
-    else
-      "one"
-    end
+  def it_or_one
+    last_beer? ? "it" : "one"
   end
 
-  def successor(number)
-    if number == 0
-      99
-    else
-      number - 1
-    end
+  def all_out?
+    bottles.zero?
+  end
+
+  def last_beer?
+    bottles == 1
   end
 end
