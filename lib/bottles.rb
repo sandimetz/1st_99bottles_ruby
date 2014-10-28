6cadec3dec376eb10be804daa86472ad582b5bdf
Katrina Owen <katrina.owen@gmail.com>
2014-10-28 04:29:19 -0700

    Extract Class: add initialize

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -63,6 +63,11 @@ class Bottles
 end
 
 class BottleNumber
+  attr_reader :number
+  def initialize(number)
+    @number = number
+  end
+
   def container(number)
     if number == 1
       "bottle"
