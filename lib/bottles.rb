b34b1569f77e65a9877414f73d2fc3a2e17de508
Katrina Owen <katrina.owen@gmail.com>
2015-02-14 18:02:18 -0800

    TDD: code for verse 2, stark conditional

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -5,10 +5,16 @@
 class Bottles
   # FIXME: no test coverage for 4..98
   def verse(number)
-    "#{number} bottles of beer on the wall, " +
-    "#{number} bottles of beer.\n" +
-    "Take one down and pass it around, " +
-    "#{number-1} #{(number-1) == 1 ? 'bottle' : 'bottles'} of beer " +
-      "on the wall.\n"
+    if number == 2
+      "2 bottles of beer on the wall, " +
+      "2 bottles of beer.\n" +
+      "Take one down and pass it around, " +
+      "1 bottle of beer on the wall.\n"
+    else
+      "#{number} bottles of beer on the wall, " +
+      "#{number} bottles of beer.\n" +
+      "Take one down and pass it around, " +
+      "#{number-1} bottles of beer on the wall.\n"
+    end
   end
 end
