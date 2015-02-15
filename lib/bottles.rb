099cb3fdbf268ee03ddcfb05adcf543d48e760a8
Katrina Owen <katrina.owen@gmail.com>
2015-02-14 18:18:03 -0800

    TDD: code for verses 99 and 98, literal strings

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -3,6 +3,18 @@
     # ...
       # ...
 class Bottles
+  def verses(_, _)
+    "99 bottles of beer on the wall, " +
+    "99 bottles of beer.\n" +
+    "Take one down and pass it around, " +
+    "98 bottles of beer on the wall.\n" +
+    "\n" +
+    "98 bottles of beer on the wall, " +
+    "98 bottles of beer.\n" +
+    "Take one down and pass it around, " +
+    "97 bottles of beer on the wall.\n"
+  end
+
   # FIXME: no test coverage for 4..98
   def verse(number)
     case number
