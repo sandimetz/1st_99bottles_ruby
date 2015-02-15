024793e354147ca4d134950ed62589803a789248
Katrina Owen <katrina.owen@gmail.com>
2015-02-14 18:17:50 -0800

    TDD: test verses 99 and 98

--- test/bottles_test.rb
+++ test/bottles_test.rb
@@ -47,4 +47,15 @@ Go to the store and buy some more, 99 bottles of beer on the wall.
     VERSE
     assert_equal expected, ::Bottles.new.verse(0)
   end
+
+  def test_a_couple_verses
+    expected = <<-VERSES
+99 bottles of beer on the wall, 99 bottles of beer.
+Take one down and pass it around, 98 bottles of beer on the wall.
+
+98 bottles of beer on the wall, 98 bottles of beer.
+Take one down and pass it around, 97 bottles of beer on the wall.
+VERSES
+    assert_equal expected, ::Bottles.new.verses(99, 98)
+  end
 end
