ed33ff935e455c7f0a3f8401f7582bac17fa7e0b
Katrina Owen <katrina.owen@gmail.com>
2015-02-14 18:17:50 -0800

    TDD: test verses 99 and 98

--- test/bottles_test.rb
+++ test/bottles_test.rb
@@ -47,4 +47,17 @@ class BottlesTest < Minitest::Test
       "99 bottles of beer on the wall.\n"
     assert_equal expected, Bottles.new.verse(0)
   end
+
+  def test_a_couple_verses
+    expected = "99 bottles of beer on the wall, " +
+      "99 bottles of beer.\n" +
+      "Take one down and pass it around, " +
+      "98 bottles of beer on the wall.\n" +
+      "\n" +
+      "98 bottles of beer on the wall, " +
+      "98 bottles of beer.\n" +
+      "Take one down and pass it around, " +
+      "97 bottles of beer on the wall.\n"
+    assert_equal expected, Bottles.new.verses(99, 98)
+  end
 end
