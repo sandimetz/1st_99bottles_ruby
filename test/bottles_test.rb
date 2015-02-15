ec732e28daf44d8bb430921412d7c3e1ff8cb87e
Katrina Owen <katrina.owen@gmail.com>
2015-02-14 18:22:38 -0800

    TDD: test for verses 2, 1, 0

--- test/bottles_test.rb
+++ test/bottles_test.rb
@@ -58,4 +58,18 @@ Take one down and pass it around, 97 bottles of beer on the wall.
 VERSES
     assert_equal expected, ::Bottles.new.verses(99, 98)
   end
+
+  def test_a_few_verses
+    expected = <<-VERSES
+2 bottles of beer on the wall, 2 bottles of beer.
+Take one down and pass it around, 1 bottle of beer on the wall.
+
+1 bottle of beer on the wall, 1 bottle of beer.
+Take it down and pass it around, no more bottles of beer on the wall.
+
+No more bottles of beer on the wall, no more bottles of beer.
+Go to the store and buy some more, 99 bottles of beer on the wall.
+VERSES
+    assert_equal expected, ::Bottles.new.verses(2, 0)
+  end
 end
