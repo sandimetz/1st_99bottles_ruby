0cb39dce57e2c1cdebfedebbc8d4543b09be285a
Katrina Owen <katrina.owen@gmail.com>
2015-05-26 09:01:38 -0600

    Coalesce: delete 2 case

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -25,11 +25,6 @@ class Bottles
       "1 bottle of beer.\n" +
       "Take it down and pass it around, " +
       "no more bottles of beer on the wall.\n"
-    when 2
-      "#{number} bottles of beer on the wall, " +
-      "#{number} bottles of beer.\n" +
-      "Take one down and pass it around, " +
-      "#{number-1} #{container(number-1)} of beer on the wall.\n"
     else
       "#{number} bottles of beer on the wall, " +
       "#{number} bottles of beer.\n" +
