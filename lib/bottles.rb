2c34fb8b246f3112210520d15f965fbc68793b4c
Katrina Owen <katrina.owen@gmail.com>
2015-05-26 08:59:25 -0600

    Coalesce: container - use it in default branch

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -34,7 +34,7 @@ class Bottles
       "#{number} bottles of beer on the wall, " +
       "#{number} bottles of beer.\n" +
       "Take one down and pass it around, " +
-      "#{number-1} bottles of beer on the wall.\n"
+      "#{number-1} #{container} of beer on the wall.\n"
     end
   end
 
