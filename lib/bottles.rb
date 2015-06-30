2d0f83d4ff608be3fc425bc3a0e5782649840f8b
Katrina Owen <katrina.owen@gmail.com>
2015-06-30 07:59:34 -0600

    Coalesce: quantity - send a message (from 'when 1')

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -24,7 +24,7 @@ class Bottles
       "#{number} #{container(number)} of beer on the wall, " +
       "#{number} #{container(number)} of beer.\n" +
       "Take #{pronoun(number)} down and pass it around, " +
-      "no more bottles of beer on the wall.\n"
+      "#{quantity} bottles of beer on the wall.\n"
     else
       "#{number} #{container(number)} of beer on the wall, " +
       "#{number} #{container(number)} of beer.\n" +
