9850b8b166c4704b9ef8c6bc5bb810a17dc87825
Katrina Owen <katrina.owen@gmail.com>
2015-06-30 08:04:53 -0600

    Coalesce: quantity - pass argument

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -24,7 +24,7 @@ class Bottles
       "#{number} #{container(number)} of beer on the wall, " +
       "#{number} #{container(number)} of beer.\n" +
       "Take #{pronoun(number)} down and pass it around, " +
-      "#{quantity} bottles of beer on the wall.\n"
+      "#{quantity(number-1)} bottles of beer on the wall.\n"
     else
       "#{number} #{container(number)} of beer on the wall, " +
       "#{number} #{container(number)} of beer.\n" +
