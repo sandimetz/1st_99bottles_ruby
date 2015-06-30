dd5257d35bc00ae7a0408a39da57560e0cb95f54
Katrina Owen <katrina.owen@gmail.com>
2015-06-30 08:58:46 -0600

    Coalesce: action - send a message

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -23,7 +23,7 @@ class Bottles
     else
       "#{quantity(number).capitalize} #{container(number)} of beer on the wall, " +
       "#{quantity(number)} #{container(number)} of beer.\n" +
-      "Take #{pronoun(number)} down and pass it around, " +
+      "#{action(number)}, " +
       "#{quantity(number-1)} #{container(number-1)} of beer on the wall.\n"
     end
   end
