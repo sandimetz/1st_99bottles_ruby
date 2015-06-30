82b7b1cad7831cc58a45dc1bdbdaa3aef3e3d87c
Katrina Owen <katrina.owen@gmail.com>
2015-06-30 07:53:32 -0600

    Coalesce: second constant->variable

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -22,7 +22,7 @@ class Bottles
       "99 bottles of beer on the wall.\n"
     when 1
       "#{number} #{container(number)} of beer on the wall, " +
-      "1 bottle of beer.\n" +
+      "#{number} bottle of beer.\n" +
       "Take it down and pass it around, " +
       "no more bottles of beer on the wall.\n"
     else
