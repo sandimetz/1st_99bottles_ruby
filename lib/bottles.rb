5fef89cb23b8fc7539c250bdd73a30487e2998ec
Katrina Owen <katrina.owen@gmail.com>
2015-12-21 10:21:31 -0700

    implement verse 2 with interpolated conditional

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,15 +1,9 @@
 class Bottles
   def verse(number)
-    if number == 2
-      "2 bottles of beer on the wall, " +
-      "2 bottles of beer.\n" +
-      "Take one down and pass it around, " +
-      "1 bottle of beer on the wall.\n"
-    else
-      "#{number} bottles of beer on the wall, " +
-      "#{number} bottles of beer.\n" +
-      "Take one down and pass it around, " +
-      "#{number-1} bottles of beer on the wall.\n"
-    end
+    "#{number} bottles of beer on the wall, " +
+    "#{number} bottles of beer.\n" +
+    "Take one down and pass it around, " +
+    "#{number-1} bottle#{'s' unless (number-1) == 1} of beer " +
+      "on the wall.\n"
   end
 end
