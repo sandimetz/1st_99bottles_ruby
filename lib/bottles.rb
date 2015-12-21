44dc7e8c57c0892de33deead63970b6c47eab522
Katrina Owen <katrina.owen@gmail.com>
2015-12-21 10:24:24 -0700

    implement verse 0

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,6 +1,11 @@
 class Bottles
   def verse(number)
     case number
+    when 0
+      "No more bottles of beer on the wall, " +
+      "no more bottles of beer.\n" +
+      "Go to the store and buy some more, " +
+      "99 bottles of beer on the wall.\n"
     when 1
       "1 bottle of beer on the wall, " +
       "1 bottle of beer.\n" +
