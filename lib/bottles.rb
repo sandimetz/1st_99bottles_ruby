72297e498c84df22d4e864e27017c8fcca2ab8fb
Katrina Owen <katrina.owen@gmail.com>
2015-12-21 10:12:22 -0700

    make first test pass

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,4 +1,8 @@
 class Bottles
   def verse(_)
+    "99 bottles of beer on the wall, " +
+    "99 bottles of beer.\n" +
+    "Take one down and pass it around, " +
+    "98 bottles of beer on the wall.\n"
   end
 end
