8326e8d755994bc0aa9976ae406eb2857a0d5cbb
Katrina Owen <katrina.owen@gmail.com>
2015-12-21 10:27:24 -0700

    implement verses 99-98 by sending a message

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,14 +1,6 @@
 class Bottles
   def verses(_, _)
-    "99 bottles of beer on the wall, " +
-    "99 bottles of beer.\n" +
-    "Take one down and pass it around, " +
-    "98 bottles of beer on the wall.\n" +
-    "\n" +
-    "98 bottles of beer on the wall, " +
-    "98 bottles of beer.\n" +
-    "Take one down and pass it around, " +
-    "97 bottles of beer on the wall.\n"
+    verse(99) + "\n" + verse(98)
   end
 
   def verse(number)
