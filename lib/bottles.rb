4a6fdd5c48294d5c66a61a8747e46df96d8de0cd
Katrina Owen <katrina.owen@gmail.com>
2015-12-21 10:26:34 -0700

    implement verses 99-98 with literal string

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,4 +1,16 @@
 class Bottles
+  def verses(_, _)
+    "99 bottles of beer on the wall, " +
+    "99 bottles of beer.\n" +
+    "Take one down and pass it around, " +
+    "98 bottles of beer on the wall.\n" +
+    "\n" +
+    "98 bottles of beer on the wall, " +
+    "98 bottles of beer.\n" +
+    "Take one down and pass it around, " +
+    "97 bottles of beer on the wall.\n"
+  end
+
   def verse(number)
     case number
     when 0
