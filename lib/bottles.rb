9aaa6ddc578eaa263b8d16b423f7926d419bfba9
Katrina Owen <katrina.owen@gmail.com>
2015-12-21 10:23:35 -0700

    implement verse 1

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,6 +1,12 @@
 class Bottles
   def verse(number)
-    if number == 2
+    case number
+    when 1
+      "1 bottle of beer on the wall, " +
+      "1 bottle of beer.\n" +
+      "Take it down and pass it around, " +
+      "no more bottles of beer on the wall.\n"
+    when 2
       "2 bottles of beer on the wall, " +
       "2 bottles of beer.\n" +
       "Take one down and pass it around, " +
