f1a50486794d35fb69fd6a2136100c4359cfbb41
Katrina Owen <katrina.owen@gmail.com>
2015-12-28 07:17:16 -0700

    extract class: forward all the messages

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -26,35 +26,19 @@ class Bottles
   end
 
   def quantity(number)
-    if number == 0
-      "no more"
-    else
-      number.to_s
-    end
+    BottleNumber.new(number).quantity(number)
   end
 
   def action(number)
-    if number == 0
-      "Go to the store and buy some more"
-    else
-      "Take #{pronoun(number)} down and pass it around"
-    end
+    BottleNumber.new(number).action(number)
   end
 
   def pronoun(number)
-    if number == 1
-      "it"
-    else
-      "one"
-    end
+    BottleNumber.new(number).pronoun(number)
   end
 
   def successor(number)
-    if number == 0
-      99
-    else
-      number - 1
-    end
+    BottleNumber.new(number).successor(number)
   end
 end
 
