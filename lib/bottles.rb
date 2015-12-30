61c63c1b81cbfed327cc502d54cb80df13768062
Katrina Owen <katrina.owen@gmail.com>
2015-12-30 11:07:31 -0700

    Add starting point for unwinding hierarchy

---
+++ lib/bottles.rb
@@ -0,0 +1,73 @@
+# ...
+  # ...
+    # ...
+      # ...
+class Bottles
+
+  def song
+    verses(99, 0)
+  end
+
+  def verses(starting, ending)
+    starting.downto(ending).collect {|i| verse(i)}.join("\n")
+  end
+
+  def verse(number)
+    bottle_number = BottleNumber.new(number)
+    next_bottle_number = BottleNumber.new(bottle_number.successor)
+
+    "#{bottle_number.quantity.capitalize} #{bottle_number.container} " +
+      "of beer on the wall, " +
+    "#{bottle_number.quantity} #{bottle_number.container} of beer.\n" +
+    "#{bottle_number.action}, " +
+    "#{next_bottle_number.quantity} #{next_bottle_number.container} " +
+      "of beer on the wall.\n"
+  end
+end
+
+class BottleNumber
+  attr_reader :number
+  def initialize(number)
+    @number = number
+  end
+
+  def container
+    if number == 1
+      "bottle"
+    else
+      "bottles"
+    end
+  end
+
+  def quantity
+    if number == 0
+      "no more"
+    else
+      number.to_s
+    end
+  end
+
+  def action
+    if number == 0
+      "Go to the store and buy some more"
+    else
+      "Take #{pronoun} down and pass it around"
+    end
+  end
+
+  def pronoun
+    if number == 1
+      "it"
+    else
+      "one"
+    end
+  end
+
+  def successor
+    if number == 0
+      99
+    else
+      number - 1
+    end
+  end
+end
