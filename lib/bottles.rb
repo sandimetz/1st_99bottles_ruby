1717a2d78519a28e69dded7a9fbb05b3653f0ce5
Katrina Owen <kytrinyx@github.com>
2016-07-17 12:53:23 -0600

    Replace hard-coded number

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -20,10 +20,10 @@ class Bottles
       "Take it down and pass it around, " +
       "no more bottles of beer on the wall.\n"
     when 2
-      "2 bottles of beer on the wall, " +
-      "2 bottles of beer.\n" +
+      "#{number} bottles of beer on the wall, " +
+      "#{number} bottles of beer.\n" +
       "Take one down and pass it around, " +
-      "1 bottle of beer on the wall.\n"
+      "#{number-1} bottle of beer on the wall.\n"
     else
       "#{number} bottles of beer on the wall, " +
       "#{number} bottles of beer.\n" +
