a33b95fb57be22c861068da9415a7894b707d971
Katrina Owen <kytrinyx@github.com>
2016-07-17 13:30:00 -0600

    Send container message from 'when 1' case

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -18,7 +18,7 @@ class Bottles
       "#{number} #{container(number)} of beer on the wall, " +
       "#{number} #{container(number)} of beer.\n" +
       "Take #{pronoun(number)} down and pass it around, " +
-      "#{quantity(number-1)} bottles of beer on the wall.\n"
+      "#{quantity(number-1)} #{container(number-1)} of beer on the wall.\n"
     else
       "#{number} #{container(number)} of beer on the wall, " +
       "#{number} #{container(number)} of beer.\n" +
