cb5827433ab63a808c3da0ec810f8f0c2dc05e90
Katrina Owen <kytrinyx@github.com>
2016-07-17 13:38:06 -0600

    Send action message from verse

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -12,12 +12,12 @@ class Bottles
     when 0
       "#{quantity(number).capitalize} #{container(number)} of beer on the wall, " +
       "#{quantity(number)} #{container(number)} of beer.\n" +
-      "Go to the store and buy some more, " +
+      "#{action(number)}, " +
       "99 bottles of beer on the wall.\n"
     else
       "#{quantity(number).capitalize} #{container(number)} of beer on the wall, " +
       "#{quantity(number)} #{container(number)} of beer.\n" +
-      "Take #{pronoun(number)} down and pass it around, " +
+      "#{action(number)}, " +
       "#{quantity(number-1)} #{container(number-1)} of beer on the wall.\n"
     end
   end
