7fad55db54ccbff2308841c501557a8cc1ad972d
Katrina Owen <kytrinyx@github.com>
2016-07-17 13:35:01 -0600

    Send quantity message from 'else' case

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -15,7 +15,7 @@ class Bottles
       "Go to the store and buy some more, " +
       "99 bottles of beer on the wall.\n"
     else
-      "#{number} #{container(number)} of beer on the wall, " +
+      "#{quantity(number).capitalize} #{container(number)} of beer on the wall, " +
       "#{number} #{container(number)} of beer.\n" +
       "Take #{pronoun(number)} down and pass it around, " +
       "#{quantity(number-1)} #{container(number-1)} of beer on the wall.\n"
