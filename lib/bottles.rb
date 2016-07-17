50b11a1ba42faf65383b4494b45dcc9c4a58703c
Katrina Owen <kytrinyx@github.com>
2016-07-17 13:11:47 -0600

    Make cases 1 and else more similar
    This fixes the low-hanging fruit in phrases 1 and 2 of the 'when 1' case
    and the 'else' case.

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -15,13 +15,13 @@ class Bottles
       "Go to the store and buy some more, " +
       "99 bottles of beer on the wall.\n"
     when 1
-      "1 bottle of beer on the wall, " +
-      "1 bottle of beer.\n" +
+      "#{number} #{container(number)} of beer on the wall, " +
+      "#{number} #{container(number)} of beer.\n" +
       "Take it down and pass it around, " +
       "no more bottles of beer on the wall.\n"
     else
-      "#{number} bottles of beer on the wall, " +
-      "#{number} bottles of beer.\n" +
+      "#{number} #{container(number)} of beer on the wall, " +
+      "#{number} #{container(number)} of beer.\n" +
       "Take one down and pass it around, " +
       "#{number-1} #{container(number-1)} of beer on the wall.\n"
     end
