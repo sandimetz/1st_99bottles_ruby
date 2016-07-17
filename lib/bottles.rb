5cc0c4ea0742600617ed4d50796032e02499cc75
Katrina Owen <kytrinyx@github.com>
2016-07-17 13:15:53 -0600

    Send pronoun message in 'else' case

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -22,7 +22,7 @@ class Bottles
     else
       "#{number} #{container(number)} of beer on the wall, " +
       "#{number} #{container(number)} of beer.\n" +
-      "Take one down and pass it around, " +
+      "Take #{pronoun} down and pass it around, " +
       "#{number-1} #{container(number-1)} of beer on the wall.\n"
     end
   end
