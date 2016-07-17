3df10b9134a10f1045495632867cab31fb411b49
Katrina Owen <kytrinyx@github.com>
2016-07-17 13:37:01 -0600

    Send quantity and container messages everywhere

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -11,12 +11,12 @@ class Bottles
     case number
     when 0
       "#{quantity(number).capitalize} #{container(number)} of beer on the wall, " +
-      "no more bottles of beer.\n" +
+      "#{quantity(number)} #{container(number)} of beer.\n" +
       "Go to the store and buy some more, " +
       "99 bottles of beer on the wall.\n"
     else
       "#{quantity(number).capitalize} #{container(number)} of beer on the wall, " +
-      "#{number} #{container(number)} of beer.\n" +
+      "#{quantity(number)} #{container(number)} of beer.\n" +
       "Take #{pronoun(number)} down and pass it around, " +
       "#{quantity(number-1)} #{container(number-1)} of beer on the wall.\n"
     end
