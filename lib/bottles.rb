1339c9bb2944b53e52be2349680401fb114f66af
Katrina Owen <kytrinyx@github.com>
2016-07-17 13:47:02 -0600

    Send successor message from 'else' case

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -18,7 +18,7 @@ class Bottles
       "#{quantity(number).capitalize} #{container(number)} of beer on the wall, " +
       "#{quantity(number)} #{container(number)} of beer.\n" +
       "#{action(number)}, " +
-      "#{quantity(number-1)} #{container(number-1)} of beer on the wall.\n"
+      "#{quantity(successor(number))} #{container(number-1)} of beer on the wall.\n"
     end
   end
 
