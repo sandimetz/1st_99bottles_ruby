45727ce0a5ef00ade361021d3355b0430dff82cc
Katrina Owen <kytrinyx@github.com>
2016-09-29 12:00:38 -0600

    Add initializer to new class

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -56,6 +56,11 @@ class Bottles
 end
 
 class BottleNumber
+  attr_reader :number
+  def initialize(number)
+    @number = number
+  end
+
   def container(number)
     if number == 1
       "bottle"
