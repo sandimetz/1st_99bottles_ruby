c8c761c2954efa4037a4b9b5a8d1cdf436ccb0ba
Katrina Owen <kytrinyx@github.com>
2016-09-29 11:59:54 -0600

    Copy (not move) methods into new class

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -56,4 +56,43 @@ class Bottles
 end
 
 class BottleNumber
+  def container(number)
+    if number == 1
+      "bottle"
+    else
+      "bottles"
+    end
+  end
+
+  def quantity(number)
+    if number == 0
+      "no more"
+    else
+      number.to_s
+    end
+  end
+
+  def action(number)
+    if number == 0
+      "Go to the store and buy some more"
+    else
+      "Take #{pronoun(number)} down and pass it around"
+    end
+  end
+
+  def pronoun(number)
+    if number == 1
+      "it"
+    else
+      "one"
+    end
+  end
+
+  def successor(number)
+    if number == 0
+      99
+    else
+      number - 1
+    end
+  end
 end
