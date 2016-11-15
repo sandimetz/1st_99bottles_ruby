3ec69695bce21bddfc8716036d052a6a1a977f86
Sandi Metz <sandi@sandimetz.com>
2016-11-15 10:28:43 -0500

    DataClump: Add to_s, use in verse template

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -16,11 +16,11 @@ class Bottles
     bottle_number      = BottleNumber.new(number)
     next_bottle_number = BottleNumber.new(bottle_number.successor)
 
-    "#{bottle_number.quantity.capitalize} #{bottle_number.container} " +
+    "#{bottle_number} ".capitalize +
       "of beer on the wall, " +
-    "#{bottle_number.quantity} #{bottle_number.container} of beer.\n" +
+    "#{bottle_number} of beer.\n" +
     "#{bottle_number.action}, " +
-    "#{next_bottle_number.quantity} #{next_bottle_number.container} " +
+    "#{next_bottle_number} " +
       "of beer on the wall.\n"
   end
 end
@@ -29,6 +29,10 @@ class BottleNumber
   attr_reader :number
   def initialize(number)
     @number = number
+  end
+
+  def to_s
+    "#{quantity} #{container}"
   end
 
   def container
