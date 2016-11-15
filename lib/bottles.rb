eb97ec42f931ddf079b6f9aaa427ea8da29afa68
Sandi Metz <sandi@sandimetz.com>
2016-11-15 12:01:50 -0500

    SixPack: Make the factory open
    Requires metaprogramming and a class naming convention.

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -24,16 +24,23 @@ end
 
 class BottleNumber
   def self.for(number)
-    case number
-    when 0
-      BottleNumber0
-    when 1
-      BottleNumber1
-    when 6
-      BottleNumber6
-    else
+    begin
+      const_get("BottleNumber#{number}")
+    rescue NameError
       BottleNumber
     end.new(number)
+
+
+    # case number
+    # when 0
+    #   BottleNumber0
+    # when 1
+    #   BottleNumber1
+    # when 6
+    #   BottleNumber6
+    # else
+    #   BottleNumber
+    # end.new(number)
   end
 
   attr_reader :number
@@ -94,7 +101,7 @@ class BottleNumber6 < BottleNumber
   def quantity
     "1"
   end
-  
+
   def container
     "six-pack"
   end
