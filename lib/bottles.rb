dfa28bdea848dc733b0c71cdb1e1f5c5ad4f9f94
Sandi Metz <sandi@sandimetz.com>
2016-11-15 11:17:25 -0500

    Hierarchy: Use factory method in verse

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -13,8 +13,8 @@ class Bottles
   end
 
   def verse(number)
-    bottle_number      = BottleNumber.new(number)
-    next_bottle_number = BottleNumber.new(bottle_number.successor)
+    bottle_number      = bottle_number_for(number)
+    next_bottle_number = bottle_number_for(bottle_number.successor)
 
     "#{bottle_number} of beer on the wall, ".capitalize +
     "#{bottle_number} of beer.\n" +
