ba211856bdcd62e9d51400c44715c19e47accd03
Sandi Metz <sandi@sandimetz.com>
2016-11-15 11:34:16 -0500

    Successor: Code we wish we had

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -15,6 +15,7 @@ class Bottles
   def verse(number)
     bottle_number      = bottle_number_for(number)
     next_bottle_number = bottle_number_for(bottle_number.successor)
+    # next_bottle_number = bottle_number.successor
 
     "#{bottle_number} of beer on the wall, ".capitalize +
     "#{bottle_number} of beer.\n" +
