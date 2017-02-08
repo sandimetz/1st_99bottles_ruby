0a5f58f7d3cf2eafae04b36e55a254d8efb1c802
Katrina Owen <kytrinyx@github.com>
2017-02-08 08:17:18 -0700

    Inline temporary variable

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -8,13 +8,12 @@ class Bottles
   end
 
   def verse(number)
-    bottle_number      = BottleNumber.for(number)
-    next_bottle_number = bottle_number.successor
+    bottle_number = BottleNumber.for(number)
 
     "#{bottle_number} of beer on the wall, ".capitalize +
     "#{bottle_number} of beer.\n" +
     "#{bottle_number.action}, " +
-    "#{next_bottle_number} of beer on the wall.\n"
+    "#{bottle_number.successor} of beer on the wall.\n"
   end
 end
 
