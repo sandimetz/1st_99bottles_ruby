15c61df14beca9fc0dbebfa656c423ecfef329fd
Katrina Owen <kytrinyx@github.com>
2017-02-08 10:47:09 -0700

    Make the easy change

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -24,6 +24,8 @@ class BottleNumber
       BottleNumber0
     when 1
       BottleNumber1
+    when 6
+      BottleNumber6
     else
       BottleNumber
     end.new(number)
@@ -80,5 +82,15 @@ class BottleNumber1 < BottleNumber
 
   def pronoun
     "it"
+  end
+end
+
+class BottleNumber6 < BottleNumber
+  def quantity
+    "1"
+  end
+
+  def container
+    "six-pack"
   end
 end
--- test/bottles_test.rb
+++ test/bottles_test.rb
@@ -354,9 +354,9 @@ Take one down and pass it around, 8 bottles of beer on the wall.
 Take one down and pass it around, 7 bottles of beer on the wall.
 
 7 bottles of beer on the wall, 7 bottles of beer.
-Take one down and pass it around, 6 bottles of beer on the wall.
+Take one down and pass it around, 1 six-pack of beer on the wall.
 
-6 bottles of beer on the wall, 6 bottles of beer.
+1 six-pack of beer on the wall, 1 six-pack of beer.
 Take one down and pass it around, 5 bottles of beer on the wall.
 
 5 bottles of beer on the wall, 5 bottles of beer.
