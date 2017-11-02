61a663542e28491cca385642bd113d87603a0339
Sandi Metz <sandi@sandimetz.com>
2017-11-02 16:38:41 -0400

    Use #to_s in phrase 1

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -11,7 +11,7 @@ class Bottles
     bottle_number = BottleNumber.new(number)
     next_bottle_number = BottleNumber.new(bottle_number.successor)
 
-    "#{bottle_number.quantity.capitalize} #{bottle_number.container} of beer on the wall, ".capitalize +
+    "#{bottle_number} of beer on the wall, ".capitalize +
     "#{bottle_number.quantity} #{bottle_number.container} of beer.\n" +
     "#{bottle_number.action}, " +
     "#{next_bottle_number.quantity} #{next_bottle_number.container} of beer on the wall.\n"
