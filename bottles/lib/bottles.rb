c7365d877140621f8786c46fce1dcd584243c1d0
Sandi Metz <sandi@sandimetz.com>
2017-11-02 16:33:06 -0400

    Use the bottle_number cache in phrase 1

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -10,7 +10,7 @@ class Bottles
   def verse(number)
     bottle_number = BottleNumber.new(number)
 
-    "#{quantity(number).capitalize} #{container(number)} of beer on the wall, " +
+    "#{bottle_number.quantity.capitalize} #{bottle_number.container} of beer on the wall, " +
     "#{quantity(number)} #{container(number)} of beer.\n" +
     "#{action(number)}, " +
     "#{quantity(successor(number))} #{container(successor(number))} of beer on the wall.\n"
