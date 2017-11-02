5c5ae844d10b7e11bd4f4a1386a9651b705c1a2b
Sandi Metz <sandi@sandimetz.com>
2017-11-02 15:05:35 -0400

    Delete obsolete 2 case.

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -13,8 +13,6 @@ class Bottles
           "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
         when 1
           "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
-        when 2
-          "#{number} bottles of beer on the wall, #{number} bottles of beer.\nTake one down and pass it around, #{number-1} #{container(number-1)} of beer on the wall.\n"
         else
           "#{number} bottles of beer on the wall, #{number} bottles of beer.\nTake one down and pass it around, #{number-1} #{container(number-1)} of beer on the wall.\n"
         end
