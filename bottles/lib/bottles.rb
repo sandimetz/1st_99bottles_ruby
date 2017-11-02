c9a0d4b1b81b174db84b2b1d23f4a1ce08028384
Sandi Metz <sandi@sandimetz.com>
2017-11-02 15:41:55 -0400

    Fix crazy indenting.
    New text editor, what can I say?

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -1,57 +1,56 @@
 class Bottles
-      def song
-        verses(99, 0)
-      end
+  def song
+    verses(99, 0)
+  end
 
-      def verses(upper, lower)
-        upper.downto(lower).map { |i| verse(i) }.join("\n")
-      end
+  def verses(upper, lower)
+    upper.downto(lower).map { |i| verse(i) }.join("\n")
+  end
 
-      def verse(number)
-        "#{quantity(number).capitalize} #{container(number)} of beer on the wall, " +
-        "#{quantity(number)} #{container(number)} of beer.\n" +
-        "#{action(number)}, " +
-        "#{quantity(successor(number))} #{container(successor(number))} of beer on the wall.\n"
-      end
+  def verse(number)
+    "#{quantity(number).capitalize} #{container(number)} of beer on the wall, " +
+    "#{quantity(number)} #{container(number)} of beer.\n" +
+    "#{action(number)}, " +
+    "#{quantity(successor(number))} #{container(successor(number))} of beer on the wall.\n"
+  end
 
-      def quantity(number)
-        if number == 0
-          "no more"
-        else
-          number.to_s
-        end
-      end
-
-      def container(number)
-        if number == 1
-          "bottle"
-         else
-          "bottles"
-         end
-      end
-
-      def action(number)
-        if number == 0
-          "Go to the store and buy some more"
-        else
-          "Take #{pronoun(number)} down and pass it around"
-        end
-      end
-
-      def pronoun(number)
-        if number == 1
-          "it"
-        else
-          "one"
-        end
-      end
-
-      def successor(number)
-        if number == 0
-          99
-        else
-          number - 1
-        end
-      end
-
+  def quantity(number)
+    if number == 0
+      "no more"
+    else
+      number.to_s
     end
+  end
+
+  def container(number)
+    if number == 1
+      "bottle"
+     else
+      "bottles"
+     end
+  end
+
+  def action(number)
+    if number == 0
+      "Go to the store and buy some more"
+    else
+      "Take #{pronoun(number)} down and pass it around"
+    end
+  end
+
+  def pronoun(number)
+    if number == 1
+      "it"
+    else
+      "one"
+    end
+  end
+
+  def successor(number)
+    if number == 0
+      99
+    else
+      number - 1
+    end
+  end
+end
