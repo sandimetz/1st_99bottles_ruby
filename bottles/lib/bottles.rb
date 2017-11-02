0d54a93a4f1e7578938c4ca1e32d808562168f41
Sandi Metz <sandi@sandimetz.com>
2017-11-02 15:35:44 -0400

    Parse and execute candidate line above case statement

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -8,6 +8,7 @@ class Bottles
       end
 
       def verse(number)
+        "#{quantity(number).capitalize} #{container(number)} of beer on the wall, #{quantity(number)} #{container(number)} of beer.\n#{action(number)}, #{quantity(successor(number))} #{container(number-1)} of beer on the wall.\n"
         case number
         when 0
           "#{quantity(number).capitalize} #{container(number)} of beer on the wall, #{quantity(number)} #{container(number)} of beer.\n#{action(number)}, #{quantity(successor(number))} #{container(number-1)} of beer on the wall.\n"
