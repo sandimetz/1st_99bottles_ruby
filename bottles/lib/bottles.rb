57e1efeae698a76c3f0f7bc26b7f5b744bf45d74
Sandi Metz <sandi@sandimetz.com>
2017-11-02 16:56:25 -0400

    Copy the #successor method

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -78,4 +78,12 @@ class BottleNumber0 < BottleNumber
   def action
     "Go to the store and buy some more"
   end
+
+  def successor
+    if number == 0
+      99
+    else
+      number - 1
+    end
+  end
 end
