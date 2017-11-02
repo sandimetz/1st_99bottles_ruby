2f82cce06f54e8382321ac515e9219ec1512eca5
Sandi Metz <sandi@sandimetz.com>
2017-11-02 16:35:55 -0400

    Delete the obsolete forwarding methods

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -16,22 +16,6 @@ class Bottles
     "#{bottle_number.action}, " +
     "#{next_bottle_number.quantity} #{next_bottle_number.container} of beer on the wall.\n"
   end
-
-  def quantity(number)
-    BottleNumber.new(number).quantity
-  end
-
-  def container(number)
-    BottleNumber.new(number).container
-  end
-
-  def action(number)
-    BottleNumber.new(number).action
-  end
-
-  def successor(number)
-    BottleNumber.new(number).successor
-  end
 end
 
 
