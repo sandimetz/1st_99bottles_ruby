e5c73a75b1fd7a9999a26bd96e85d58fe6d6e9cc
Sandi Metz <sandi@sandimetz.com>
2017-11-02 16:56:00 -0400

    Delete obsolete code

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -50,11 +50,6 @@ class BottleNumber
   end
 
   def action
-    if number == 0
-      "Go to the store and buy some more"
-    else
-      "Take #{pronoun} down and pass it around"
-    end
     "Take #{pronoun} down and pass it around"
   end
 
