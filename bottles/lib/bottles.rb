91ffe18d74a1502d52d4612582d91cc3be418e26
Sandi Metz <sandi@sandimetz.com>
2017-11-02 15:06:07 -0400

    Begin eliminating differences from 'else' and '1' case.

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -12,7 +12,7 @@ class Bottles
         when 0
           "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
         when 1
-          "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
+          "#{number} bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
         else
           "#{number} bottles of beer on the wall, #{number} bottles of beer.\nTake one down and pass it around, #{number-1} #{container(number-1)} of beer on the wall.\n"
         end
