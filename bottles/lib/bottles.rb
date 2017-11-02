54b217f02ec378f0091a43dad7c093ac4540de2e
Sandi Metz <sandi@sandimetz.com>
2017-11-02 14:54:11 -0400

    Send #container in the false branch.

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -16,7 +16,7 @@ class Bottles
         when 2
           "#{number} bottles of beer on the wall, #{number} bottles of beer.\nTake one down and pass it around, #{number-1} bottle of beer on the wall.\n"
         else
-          "#{number} bottles of beer on the wall, #{number} bottles of beer.\nTake one down and pass it around, #{number-1} bottles of beer on the wall.\n"
+          "#{number} bottles of beer on the wall, #{number} bottles of beer.\nTake one down and pass it around, #{number-1} #{container} of beer on the wall.\n"
         end
       end
 
