1ba7cf0d46f8c0caca366032bd8d0a287e05f924
Sandi Metz <sandi@sandimetz.com>
2017-11-02 15:25:08 -0400

    Use #quantity in the else case.

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -12,7 +12,7 @@ class Bottles
         when 0
           "#{quantity(number).capitalize} bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
         else
-          "#{number} #{container(number)} of beer on the wall, #{number} #{container(number)} of beer.\nTake #{pronoun(number)} down and pass it around, #{quantity(number-1)} #{container(number-1)} of beer on the wall.\n"
+          "#{quantity(number).capitalize} #{container(number)} of beer on the wall, #{number} #{container(number)} of beer.\nTake #{pronoun(number)} down and pass it around, #{quantity(number-1)} #{container(number-1)} of beer on the wall.\n"
         end
       end
 
