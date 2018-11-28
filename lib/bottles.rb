91074c9fa02accad626fb50c9796baf75cc29f92
Sandi Metz <sandi@sandimetz.com>
2018-11-28 17:23:41 -0500

    factory via dictionary/hash

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -24,11 +24,13 @@ end
 
 class BottleNumber
   def self.for(number)
-    begin
-      const_get("BottleNumber#{number}")
-    rescue NameError
-      BottleNumber
-    end.new(number)
+    Hash.new(BottleNumber).merge(0 => BottleNumber0, 1 => BottleNumber1, 6 => BottleNumber6)[number].new(number)
+
+    # begin
+    #   const_get("BottleNumber#{number}")
+    # rescue NameError
+    #   BottleNumber
+    # end.new(number)
 
 
     # case number
