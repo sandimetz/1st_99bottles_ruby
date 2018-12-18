4a120c7f81c8a057bbeebee8ef439fb9762f099d
Sandi Metz <sandi@sandimetz.com>
2018-12-18 13:53:14 -0500

    update first test to use BottleVerse

--- test/bottles_test.rb
+++ test/bottles_test.rb
@@ -9,7 +9,7 @@ class BottleVerseTest < Minitest::Test
       99 bottles of beer on the wall, 99 bottles of beer.
       Take one down and pass it around, 98 bottles of beer on the wall.
     VERSE
-    assert_equal expected, Bottles.new.verse(99)
+    assert_equal expected, BottleVerse.new(99).lyrics
   end
 end
 
