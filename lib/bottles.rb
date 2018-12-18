442588f22d8dee0d3961f77b14b624221934cd8c
Sandi Metz <sandi@sandimetz.com>
2018-12-18 12:56:35 -0500

    use the result

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -9,12 +9,12 @@ class Bottles
 
   def verse(number)
     BottleVerse.new(number).verse(number)
-    bottle_number = BottleNumber.for(number)
+    # bottle_number = BottleNumber.for(number)
 
-    "#{bottle_number} of beer on the wall, ".capitalize +
-    "#{bottle_number} of beer.\n" +
-    "#{bottle_number.action}, " +
-    "#{bottle_number.successor} of beer on the wall.\n"
+    # "#{bottle_number} of beer on the wall, ".capitalize +
+    # "#{bottle_number} of beer.\n" +
+    # "#{bottle_number.action}, " +
+    # "#{bottle_number.successor} of beer on the wall.\n"
   end
 end
 
