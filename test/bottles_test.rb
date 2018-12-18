3b762d8a3cb2276b7d0e6d0099fbfbe0501ffd29
Sandi Metz <sandi@sandimetz.com>
2018-12-18 13:49:28 -0500

    copy one test to BottleVerse

--- test/bottles_test.rb
+++ test/bottles_test.rb
@@ -3,6 +3,16 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require_relative '../lib/bottles'
 
+class BottleVerseTest < Minitest::Test
+  def test_the_first_verse
+    expected = <<~VERSE
+      99 bottles of beer on the wall, 99 bottles of beer.
+      Take one down and pass it around, 98 bottles of beer on the wall.
+    VERSE
+    assert_equal expected, Bottles.new.verse(99)
+  end
+end
+
 class BottlesTest < Minitest::Test
   def test_the_first_verse
     expected = <<~VERSE
