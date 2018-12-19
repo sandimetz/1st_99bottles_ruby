2858e431b4bf4daf07a5eb73e578e72c56b396f4
Sandi Metz <sandi@sandimetz.com>
2018-12-19 05:31:13 -0500

    dup Bottles as CountDownSong

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,3 +1,22 @@
+class CountDownSong
+  attr_reader :verse_template
+  def initialize(verse_template: BottleVerse)
+    @verse_template = verse_template
+  end
+
+  def song
+    verses(99, 0)
+  end
+
+  def verses(starting, ending)
+    starting.downto(ending).collect {|i| verse(i)}.join("\n")
+  end
+
+  def verse(number)
+    verse_template.new(number).lyrics
+  end
+end
+
 class Bottles
   attr_reader :verse_template
   def initialize(verse_template: BottleVerse)
