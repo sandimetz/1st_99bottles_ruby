817c9d07205b7782acc05271b2587e6f10cf17ad
Sandi Metz <sandi@sandimetz.com>
2018-12-21 15:15:33 -0500

    delete obsolete initialize method

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -29,11 +29,6 @@ class BottleVerse
     @bottle_number = bottle_number
   end
 
-  def initialize(number)
-    @number = number
-    @bottle_number = number
-  end
-
   def lyrics
     "#{bottle_number} of beer on the wall, ".capitalize +
     "#{bottle_number} of beer.\n" +
