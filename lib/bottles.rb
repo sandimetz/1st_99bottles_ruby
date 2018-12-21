e4ad0fac76d01bc61c327bc447cb1809c9966a04
Sandi Metz <sandi@sandimetz.com>
2018-12-21 15:05:17 -0500

    remove type check

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -30,8 +30,7 @@ class BottleVerse
   end
 
   def lyrics
-    bottle_number =
-      (number.is_a?(BottleNumber) ? number : BottleNumber.for(number))
+    bottle_number = number
 
     "#{bottle_number} of beer on the wall, ".capitalize +
     "#{bottle_number} of beer.\n" +
