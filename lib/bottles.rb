76489eed584b6b46fef5047c749616164bcc5eae
Sandi Metz <sandi@sandimetz.com>
2019-04-12 10:11:16 -0400

    fix indents

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,102 +1,102 @@
 class Bottles
 
-def song
-  verses(99, 0)
-end
-
-def verses(starting, ending)
-  starting.downto(ending).collect {|i| verse(i)}.join("\n")
-end
-
-def verse(number)
-  "#{quantity(number).capitalize} #{container(number)} " +
-    "of beer on the wall, " +
-  "#{quantity(number)} #{container(number)} of beer.\n" +
-  "#{action(number)}, " +
-  "#{quantity(successor(number))} #{container(successor(number))} " +
-    "of beer on the wall.\n"
-end
-
-def quantity(number)
-  BottleNumber.new(number).quantity(number)
-end
-
-def container(number)
-  if number == 1
-    "bottle"
-  else
-    "bottles"
+  def song
+    verses(99, 0)
   end
-end
 
-def action(number)
-  if number == 0
-    "Go to the store and buy some more"
-  else
-    "Take #{pronoun(number)} down and pass it around"
+  def verses(starting, ending)
+    starting.downto(ending).collect {|i| verse(i)}.join("\n")
   end
-end
 
-def pronoun(number)
-  if number == 1
-    "it"
-  else
-    "one"
+  def verse(number)
+    "#{quantity(number).capitalize} #{container(number)} " +
+      "of beer on the wall, " +
+    "#{quantity(number)} #{container(number)} of beer.\n" +
+    "#{action(number)}, " +
+    "#{quantity(successor(number))} #{container(successor(number))} " +
+      "of beer on the wall.\n"
   end
-end
 
-def successor(number)
-  if number == 0
-    99
-  else
-    number - 1
+  def quantity(number)
+    BottleNumber.new(number).quantity(number)
   end
-end
+
+  def container(number)
+    if number == 1
+      "bottle"
+    else
+      "bottles"
+    end
+  end
+
+  def action(number)
+    if number == 0
+      "Go to the store and buy some more"
+    else
+      "Take #{pronoun(number)} down and pass it around"
+    end
+  end
+
+  def pronoun(number)
+    if number == 1
+      "it"
+    else
+      "one"
+    end
+  end
+
+  def successor(number)
+    if number == 0
+      99
+    else
+      number - 1
+    end
+  end
 end
 
 class BottleNumber
-attr_reader :number
-def initialize(number)
-  @number = number
-end
-
-def quantity(number)
-  if number == 0
-    "no more"
-  else
-    number.to_s
+  attr_reader :number
+  def initialize(number)
+    @number = number
   end
-end
 
-def container(number)
-  if number == 1
-    "bottle"
-  else
-    "bottles"
+  def quantity(number)
+    if number == 0
+      "no more"
+    else
+      number.to_s
+    end
   end
-end
 
-def action(number)
-  if number == 0
-    "Go to the store and buy some more"
-  else
-    "Take #{pronoun(number)} down and pass it around"
+  def container(number)
+    if number == 1
+      "bottle"
+    else
+      "bottles"
+    end
   end
-end
 
-def pronoun(number)
-  if number == 1
-    "it"
-  else
-    "one"
+  def action(number)
+    if number == 0
+      "Go to the store and buy some more"
+    else
+      "Take #{pronoun(number)} down and pass it around"
+    end
   end
-end
 
-def successor(number)
-  if number == 0
-    99
-  else
-    number - 1
+  def pronoun(number)
+    if number == 1
+      "it"
+    else
+      "one"
+    end
   end
-end
+
+  def successor(number)
+    if number == 0
+      99
+    else
+      number - 1
+    end
+  end
 end
