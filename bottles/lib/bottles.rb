5855977bd489bf3f4d5e106ae92e6b2da296d7ed
TJ Stankus <tjstankus@gmail.com>
2019-05-22 12:14:26 -0400

    Write the initialize we want

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -27,9 +27,8 @@ class BottleVerse
 
   attr_reader :number, :bottle_number
 
-  def initialize(number)
-    @number = number
-    @bottle_number = number
+  def initialize(bottle_number)
+    @bottle_number = bottle_number
   end
 
   def initialize(number)
