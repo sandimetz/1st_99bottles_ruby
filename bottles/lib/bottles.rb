c6404048e3ab0166162bcac571a01406cb316148
TJ Stankus <tjstankus@gmail.com>
2019-05-22 10:26:30 -0400

    Delete unnecessary code

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -9,13 +9,6 @@ class Bottles
 
   def verse(number)
     BottleVerse.new(number).lyrics
-
-    bottle_number = BottleNumber.for(number)
-
-    "#{bottle_number} of beer on the wall, ".capitalize +
-    "#{bottle_number} of beer.\n" +
-    "#{bottle_number.action}, " +
-    "#{bottle_number.successor} of beer on the wall.\n"
   end
 end
 
