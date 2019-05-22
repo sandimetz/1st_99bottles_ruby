50852d8041fc45f822159b2692bd6ac7e2ab2b83
TJ Stankus <tjstankus@gmail.com>
2019-05-22 12:07:36 -0400

    Write wishful thinking test
    test_the_whole_song is not executed, so this does not break.

--- bottles/test/bottles_test.rb
+++ bottles/test/bottles_test.rb
@@ -65,6 +65,21 @@ class CountdownSongTest < Minitest::Test
   end
 
   def test_the_whole_song
+    expected =
+      "This is verse 47.\n" +
+      "\n" +
+      "This is verse 46.\n" +
+      "\n" +
+      "This is verse 45.\n" +
+      "\n" +
+      "This is verse 44.\n" +
+      "\n" +
+      "This is verse 43.\n"
+    assert_equal expected,
+      CountdownSong.new(verse_template: FakeVerse, max: 47, min: 43).song
+  end
+
+  def test_the_whole_song
     expected = <<~SONG
       99 bottles of beer on the wall, 99 bottles of beer.
       Take one down and pass it around, 98 bottles of beer on the wall.
