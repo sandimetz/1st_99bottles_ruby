8cf76691511027afb273739a0f312dff51ddbf85
TJ Stankus <tjstankus@gmail.com>
2019-05-22 10:37:35 -0400

    Remove duplicate test

--- bottles/test/bottles_test.rb
+++ bottles/test/bottles_test.rb
@@ -12,14 +12,6 @@ class BottleVerseTest < Minitest::Test
 end
 
 class BottlesTest < Minitest::Test
-  def test_the_first_verse
-    expected = "99 bottles of beer on the wall, " +
-      "99 bottles of beer.\n" +
-      "Take one down and pass it around, " +
-      "98 bottles of beer on the wall.\n"
-    assert_equal expected, Bottles.new.verse(99)
-  end
-
   def test_another_verse
     expected = "3 bottles of beer on the wall, " +
       "3 bottles of beer.\n" +
