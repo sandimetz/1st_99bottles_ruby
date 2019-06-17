85643347a4576d690d33294b8f5bc1e9a270a9a9
TJ Stankus <tjstankus@gmail.com>
2019-06-17 14:51:06 -0400

    Comment out old code
    This better maintains the narrative, since the previous example had these
    lines commented out.

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -11,12 +11,12 @@ class Bottles
     BottleVerse.new(number).verse(number)
     # BottleVerse.new(number).lyrics
 
-    bottle_number = BottleNumber.for(number)
+    # bottle_number = BottleNumber.for(number)
 
-    "#{bottle_number} of beer on the wall, ".capitalize +
-    "#{bottle_number} of beer.\n" +
-    "#{bottle_number.action}, " +
-    "#{bottle_number.successor} of beer on the wall.\n"
+    # "#{bottle_number} of beer on the wall, ".capitalize +
+    # "#{bottle_number} of beer.\n" +
+    # "#{bottle_number.action}, " +
+    # "#{bottle_number.successor} of beer on the wall.\n"
   end
 end
 
