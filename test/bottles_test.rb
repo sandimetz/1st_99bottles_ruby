3543df1c9796550b41a0aabeea63a08c4b1f1541
TJ Stankus <tjstankus@gmail.com>
2019-09-16 15:30:10 -0400

    Test BottleVerse for all single verse tests

--- test/bottles_test.rb
+++ test/bottles_test.rb
@@ -17,7 +17,7 @@ class BottleVerseTest < Minitest::Test
       89 bottles of beer on the wall, 89 bottles of beer.
       Take one down and pass it around, 88 bottles of beer on the wall.
     VERSE
-    assert_equal expected, Bottles.new.verse(89)
+    assert_equal expected, BottleVerse.lyrics(89)
   end
 
   def test_verse_2
@@ -25,7 +25,7 @@ class BottleVerseTest < Minitest::Test
       2 bottles of beer on the wall, 2 bottles of beer.
       Take one down and pass it around, 1 bottle of beer on the wall.
     VERSE
-    assert_equal expected, Bottles.new.verse(2)
+    assert_equal expected, BottleVerse.lyrics(2)
   end
 
   def test_verse_1
@@ -33,7 +33,7 @@ class BottleVerseTest < Minitest::Test
       1 bottle of beer on the wall, 1 bottle of beer.
       Take it down and pass it around, no more bottles of beer on the wall.
     VERSE
-    assert_equal expected, Bottles.new.verse(1)
+    assert_equal expected, BottleVerse.lyrics(1)
   end
 
   def test_verse_0
@@ -41,7 +41,7 @@ class BottleVerseTest < Minitest::Test
       No more bottles of beer on the wall, no more bottles of beer.
       Go to the store and buy some more, 99 bottles of beer on the wall.
     VERSE
-    assert_equal expected, Bottles.new.verse(0)
+    assert_equal expected, BottleVerse.lyrics(0)
   end
 end
 
