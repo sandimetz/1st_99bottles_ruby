1fb9e8da34d77be374d44249bc61bea15ab53d30
TJ Stankus <tjstankus@gmail.com>
2019-09-16 15:49:31 -0400

    Arrange tests in same order as production code

--- test/bottles_test.rb
+++ test/bottles_test.rb
@@ -3,48 +3,6 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require_relative '../lib/bottles'
 
-class BottleVerseTest < Minitest::Test
-  def test_the_first_verse
-    expected = <<~VERSE
-      99 bottles of beer on the wall, 99 bottles of beer.
-      Take one down and pass it around, 98 bottles of beer on the wall.
-    VERSE
-    assert_equal expected, BottleVerse.lyrics(99)
-  end
-
-  def test_another_verse
-    expected = <<~VERSE
-      3 bottles of beer on the wall, 3 bottles of beer.
-      Take one down and pass it around, 2 bottles of beer on the wall.
-    VERSE
-    assert_equal expected, BottleVerse.lyrics(3)
-  end
-
-  def test_verse_2
-    expected = <<~VERSE
-      2 bottles of beer on the wall, 2 bottles of beer.
-      Take one down and pass it around, 1 bottle of beer on the wall.
-    VERSE
-    assert_equal expected, BottleVerse.lyrics(2)
-  end
-
-  def test_verse_1
-    expected = <<~VERSE
-      1 bottle of beer on the wall, 1 bottle of beer.
-      Take it down and pass it around, no more bottles of beer on the wall.
-    VERSE
-    assert_equal expected, BottleVerse.lyrics(1)
-  end
-
-  def test_verse_0
-    expected = <<~VERSE
-      No more bottles of beer on the wall, no more bottles of beer.
-      Go to the store and buy some more, 99 bottles of beer on the wall.
-    VERSE
-    assert_equal expected, BottleVerse.lyrics(0)
-  end
-end
-
 class BottlesTest < Minitest::Test
   def test_a_couple_verses
     expected = <<~VERSES
@@ -374,5 +332,47 @@ class BottlesTest < Minitest::Test
       Go to the store and buy some more, 99 bottles of beer on the wall.
     SONG
     assert_equal expected, Bottles.new.song
+  end
+end
+
+class BottleVerseTest < Minitest::Test
+  def test_the_first_verse
+    expected = <<~VERSE
+      99 bottles of beer on the wall, 99 bottles of beer.
+      Take one down and pass it around, 98 bottles of beer on the wall.
+    VERSE
+    assert_equal expected, BottleVerse.lyrics(99)
+  end
+
+  def test_another_verse
+    expected = <<~VERSE
+      3 bottles of beer on the wall, 3 bottles of beer.
+      Take one down and pass it around, 2 bottles of beer on the wall.
+    VERSE
+    assert_equal expected, BottleVerse.lyrics(3)
+  end
+
+  def test_verse_2
+    expected = <<~VERSE
+      2 bottles of beer on the wall, 2 bottles of beer.
+      Take one down and pass it around, 1 bottle of beer on the wall.
+    VERSE
+    assert_equal expected, BottleVerse.lyrics(2)
+  end
+
+  def test_verse_1
+    expected = <<~VERSE
+      1 bottle of beer on the wall, 1 bottle of beer.
+      Take it down and pass it around, no more bottles of beer on the wall.
+    VERSE
+    assert_equal expected, BottleVerse.lyrics(1)
+  end
+
+  def test_verse_0
+    expected = <<~VERSE
+      No more bottles of beer on the wall, no more bottles of beer.
+      Go to the store and buy some more, 99 bottles of beer on the wall.
+    VERSE
+    assert_equal expected, BottleVerse.lyrics(0)
   end
 end
