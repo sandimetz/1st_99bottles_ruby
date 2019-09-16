5d79b9f25b9cb6cab18a6e00a634062fc63d8ba8
TJ Stankus <tjstankus@gmail.com>
2019-09-16 14:30:44 -0400

    Delete old test for first verse

--- test/bottles_test.rb
+++ test/bottles_test.rb
@@ -14,14 +14,6 @@ class BottleVerseTest < Minitest::Test
 end
 
 class BottlesTest < Minitest::Test
-  def test_the_first_verse
-    expected = <<~VERSE
-      99 bottles of beer on the wall, 99 bottles of beer.
-      Take one down and pass it around, 98 bottles of beer on the wall.
-    VERSE
-    assert_equal expected, Bottles.new.verse(99)
-  end
-
   def test_another_verse
     expected = <<~VERSE
       89 bottles of beer on the wall, 89 bottles of beer.
