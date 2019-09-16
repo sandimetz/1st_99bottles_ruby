acc7501e0d46409c78066cd522623fd2f1bfc651
TJ Stankus <tjstankus@gmail.com>
2019-09-16 14:34:05 -0400

    Copy single verse tests to BottleVerseTest

--- test/bottles_test.rb
+++ test/bottles_test.rb
@@ -11,6 +11,38 @@ class BottleVerseTest < Minitest::Test
     VERSE
     assert_equal expected, BottleVerse.lyrics(99)
   end
+
+  def test_another_verse
+    expected = <<~VERSE
+      89 bottles of beer on the wall, 89 bottles of beer.
+      Take one down and pass it around, 88 bottles of beer on the wall.
+    VERSE
+    assert_equal expected, Bottles.new.verse(89)
+  end
+
+  def test_verse_2
+    expected = <<~VERSE
+      2 bottles of beer on the wall, 2 bottles of beer.
+      Take one down and pass it around, 1 bottle of beer on the wall.
+    VERSE
+    assert_equal expected, Bottles.new.verse(2)
+  end
+
+  def test_verse_1
+    expected = <<~VERSE
+      1 bottle of beer on the wall, 1 bottle of beer.
+      Take it down and pass it around, no more bottles of beer on the wall.
+    VERSE
+    assert_equal expected, Bottles.new.verse(1)
+  end
+
+  def test_verse_0
+    expected = <<~VERSE
+      No more bottles of beer on the wall, no more bottles of beer.
+      Go to the store and buy some more, 99 bottles of beer on the wall.
+    VERSE
+    assert_equal expected, Bottles.new.verse(0)
+  end
 end
 
 class BottlesTest < Minitest::Test
