e67cc298dcd3c4792db2ed1721165dacbe52cbfe
TJ Stankus <tjstankus@gmail.com>
2019-09-24 20:43:23 -0400

    Copy Bottles to CountdownSong

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,3 +1,23 @@
+class CountdownSong
+  attr_reader :verse_template
+
+  def initialize(verse_template: BottleVerse)
+    @verse_template = verse_template
+  end
+
+  def song
+    verses(99, 0)
+  end
+
+  def verses(starting, ending)
+    starting.downto(ending).collect {|i| verse(i)}.join("\n")
+  end
+
+  def verse(number)
+    verse_template.lyrics(number)
+  end
+end
+
 class Bottles
   attr_reader :verse_template
 
