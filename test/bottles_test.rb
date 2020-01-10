1542928432d03dee0dac5082eb1d8f221be46f47
TJ Stankus <tjstankus@gmail.com>
2020-01-10 09:23:10 -0500

    Create test class for BottleVerse

--- test/bottles_test.rb
+++ test/bottles_test.rb
@@ -3,6 +3,9 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require_relative '../lib/bottles'
 
+class BottleVerseTest < Minitest::Test
+end
+
 class BottlesTest < Minitest::Test
   def test_the_first_verse
     expected = "99 bottles of beer on the wall, " +
