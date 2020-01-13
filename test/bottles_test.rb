17d6813e9132d21805fd9d0df76b4378bc12956e
TJ Stankus <tjstankus@gmail.com>
2020-01-13 08:28:43 -0500

    Delete individual verse tests from BottlesTest
    These tests have been moved to where they belong in BottleVerseTest.

--- test/bottles_test.rb
+++ test/bottles_test.rb
@@ -46,38 +46,6 @@ class BottleVerseTest < Minitest::Test
 end
 
 class BottlesTest < Minitest::Test
-  def test_another_verse
-    expected = "3 bottles of beer on the wall, " +
-      "3 bottles of beer.\n" +
-      "Take one down and pass it around, " +
-      "2 bottles of beer on the wall.\n"
-    assert_equal expected, Bottles.new.verse(3)
-  end
-
-  def test_verse_2
-    expected = "2 bottles of beer on the wall, " +
-      "2 bottles of beer.\n" +
-      "Take one down and pass it around, " +
-      "1 bottle of beer on the wall.\n"
-    assert_equal expected, Bottles.new.verse(2)
-  end
-
-  def test_verse_1
-    expected = "1 bottle of beer on the wall, " +
-      "1 bottle of beer.\n" +
-      "Take it down and pass it around, " +
-      "no more bottles of beer on the wall.\n"
-    assert_equal expected, Bottles.new.verse(1)
-  end
-
-  def test_verse_0
-    expected = "No more bottles of beer on the wall, " +
-      "no more bottles of beer.\n" +
-      "Go to the store and buy some more, " +
-      "99 bottles of beer on the wall.\n"
-    assert_equal expected, Bottles.new.verse(0)
-  end
-
   def test_a_couple_verses
     expected = "99 bottles of beer on the wall, " +
       "99 bottles of beer.\n" +
