e9048460a11c3e635cd30d0b2f81b211a236b0ab
TJ Stankus <tjstankus@gmail.com>
2020-01-17 15:07:12 -0500

    Make first verse test name consistent, symmetrical

--- test/bottles_test.rb
+++ test/bottles_test.rb
@@ -4,7 +4,7 @@ require 'minitest/pride'
 require_relative '../lib/bottles'
 
 class BottleVerseTest < Minitest::Test
-  def test_the_first_verse
+  def test_verse_general_rule_upper_bound
     expected = "99 bottles of beer on the wall, " +
       "99 bottles of beer.\n" +
       "Take one down and pass it around, " +
