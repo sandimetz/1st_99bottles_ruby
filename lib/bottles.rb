9a34c490d49cb57c3af07128b779f326317c4901
TJ Stankus <tjstankus@gmail.com>
2020-01-28 14:26:48 -0500

    Remove unnecessary class
    This completes the refactoring to change the Bottles class name to
    CountdownSong.

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -18,26 +18,6 @@ class CountdownSong
   end
 end
 
-class Bottles
-  attr_reader :verse_template
-
-  def initialize(verse_template: BottleVerse)
-    @verse_template = verse_template
-  end
-
-  def song
-    verses(99, 0)
-  end
-
-  def verses(upper, lower)
-    upper.downto(lower).map { |i| verse(i) }.join("\n")
-  end
-
-  def verse(number)
-    verse_template.lyrics(number)
-  end
-end
-
 class BottleVerse
   def self.lyrics(number)
     new(BottleNumber.for(number)).lyrics
