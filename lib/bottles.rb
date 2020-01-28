d27331594a5fbc3925d00c3167887a5361f31abb
TJ Stankus <tjstankus@gmail.com>
2020-01-28 14:07:44 -0500

    Duplicate Bottles as CountdownSong

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,3 +1,23 @@
+class CountdownSong
+  attr_reader :verse_template
+
+  def initialize(verse_template: BottleVerse)
+    @verse_template = verse_template
+  end
+
+  def song
+    verses(99, 0)
+  end
+
+  def verses(upper, lower)
+    upper.downto(lower).map { |i| verse(i) }.join("\n")
+  end
+
+  def verse(number)
+    verse_template.lyrics(number)
+  end
+end
+
 class Bottles
   attr_reader :verse_template
 
