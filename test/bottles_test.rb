5e6194e12c99e77a221861fa410a2b0ffaaf10a2
TJ Stankus <tjstankus@gmail.com>
2020-02-04 13:44:55 -0500

    Delete old test

--- test/bottles_test.rb
+++ test/bottles_test.rb
@@ -79,19 +79,6 @@ class CountdownSongTest < Minitest::Test
       .verses(99, 97)
   end
 
-  def test_a_couple_verses
-    expected = "99 bottles of beer on the wall, " +
-      "99 bottles of beer.\n" +
-      "Take one down and pass it around, " +
-      "98 bottles of beer on the wall.\n" +
-      "\n" +
-      "98 bottles of beer on the wall, " +
-      "98 bottles of beer.\n" +
-      "Take one down and pass it around, " +
-      "97 bottles of beer on the wall.\n"
-    assert_equal expected, CountdownSong.new.verses(99, 98)
-  end
-
   def test_a_few_verses
     expected = "2 bottles of beer on the wall, " +
       "2 bottles of beer.\n" +
